00333899c6ec0c5bb7594e457411fee4286d84fe
mehulpal12 <89767204+mehulpal12@users.noreply.github.com>
2026-01-13 22:28:15 +0530

    error2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,15 @@
-import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
-const connectionString = `${process.env.DATABASE_URL}`
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['error'],
+  })
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
-
-export { prisma }
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
